e2ea305795ef830b917a31326e1f1da7a0fd9716
Andreia Machion <amachion@gmail.com>
2026-05-08 12:17:15 -0300

    aula 08_05 remoção nós ABB - t2_s1

--- 2026_1_lab_t2_s1/abb/ABB.java
+++ 2026_1_lab_t2_s1/abb/ABB.java
@@ -66,4 +66,94 @@ public class ABB {
         int alturaDireita = alturaRec(atual.getDireita());
         return alturaDireita > alturaEsquerda ? alturaDireita + 1 : alturaEsquerda + 1;
     }
+    public boolean remove (int info) {
+        if (arvoreVazia()) return false;
+        if (info == raiz.getInfo()) {
+            if (raiz.getEsquerda() == null && raiz.getDireita() == null) {//raiz não tem filhos
+                raiz = null;
+                return true;
+            }
+            if (raiz.getDireita() == null) {//só tem filhos na esquerda
+                raiz = raiz.getEsquerda();
+                return true;
+            }
+            if (raiz.getEsquerda() == null) {//só tem filhos na direita
+                raiz = raiz.getDireita();
+                return true;
+            }
+            //a raiz tem 2 filhos
+            No suc = sucessor(raiz);
+            suc.setEsquerda(raiz.getEsquerda());
+            raiz = suc;
+            return true;
+        }
+        else {
+            if (info > raiz.getInfo())
+                return removeRec (info, raiz.getDireita(), raiz, true);
+            else
+                return removeRec (info, raiz.getEsquerda(), raiz, false);
+        }
+    }
+    private boolean removeRec (int info, No atual, No pai, boolean eFilhoDireita) {
+        if (atual == null) return false; //info não existe na árvore
+        if (info == atual.getInfo()) {
+            if (atual.getDireita() == null && atual.getEsquerda() == null) {//atual não tem filhos
+                if (eFilhoDireita) {
+                    pai.setDireita(null);
+                }
+                else {
+                    pai.setEsquerda(null);
+                }
+            }
+            else if (atual.getDireita() == null) {//só tem filho da esquerda
+                if (eFilhoDireita) {
+                    pai.setDireita(atual.getEsquerda());
+                }
+                else {
+                    pai.setEsquerda(atual.getEsquerda());
+                }
+            }
+            else if (atual.getEsquerda() == null) {//só tem filho da direita
+                if (eFilhoDireita) {
+                    pai.setDireita(atual.getDireita());
+                }
+                else {
+                    pai.setEsquerda(atual.getDireita());
+                }
+            }
+            else {//tem 2 filhos
+                No suc = sucessor(atual);
+                suc.setEsquerda(atual.getEsquerda());
+                if (eFilhoDireita) {
+                    pai.setDireita(suc);
+                }
+                else {
+                    pai.setEsquerda(suc);
+                }
+            }
+            return true;
+        }
+        else if (info > atual.getInfo()) {
+            return removeRec(info, atual.getDireita(), atual, true);
+        }
+        else {
+            return removeRec(info, atual.getEsquerda(), atual, false);
+        }
+    }
+
+    private No sucessor (No atual) {
+        No pai = atual;
+        No suc = atual.getDireita();
+        No runner = suc.getEsquerda();
+        while (runner != null) {
+            pai = suc;
+            suc = runner;
+            runner = runner.getEsquerda();
+        }
+        if (suc != atual.getDireita()) {
+            pai.setEsquerda(suc.getDireita());
+            suc.setDireita(atual.getDireita());
+        }
+        return suc;
+    }
 }
--- 2026_1_lab_t2_s1/abb/TesteABB.java
+++ 2026_1_lab_t2_s1/abb/TesteABB.java
@@ -4,13 +4,21 @@ public class TesteABB {
     public static void main(String[] args) {
         Random random = new Random();
         ABB abb = new ABB();
-        for (int i=1; i<=5000; i++) {
-            int n = random.nextInt(10000);
-            //System.out.print(n + " ");
+        for (int i=1; i<=50; i++) {
+            int n = random.nextInt(100);
+            System.out.print(n + " ");
             abb.insere(n);
         }
-        //System.out.println("\n\n" + abb);
+        System.out.println("\n\n" + abb);
         System.out.println("a arvore tem " + abb.contaNos() + " nos");
         System.out.println("altura = " + abb.altura());
+        int n = random.nextInt(100);
+        if (abb.remove(n)) {
+            System.out.println("\n\n" + abb);
+            System.out.println("a arvore tem " + abb.contaNos() + " nos");
+        }
+        else {
+            System.out.println(n + " nao encontrado");
+        }
     }
 }
